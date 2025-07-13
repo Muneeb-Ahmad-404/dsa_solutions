cdae739164763d08a835acb1eac5494a6cc205d8
Muneeb Ahmad <muneebahmad2005p@gmail.com>
2025-07-13 13:07:36 +0500

    Solved Contains Duplicate LC 217

---
+++ Arrays & Hashing/Contains_Duplicate_LC_217/Contains_Duplicate_LC_217.py
@@ -0,0 +1,53 @@
+def bruteforce(nums):
+    has_duplicate = False
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] == nums[j]:
+                has_duplicate = True
+                break
+    return has_duplicate
+
+
+def set_approach1(nums):
+    return (len(set(nums)) != len(nums))
+
+def set_approach2(nums):
+    unique_set = set()
+    for num in nums:
+        if num in unique_set:
+            return True
+        unique_set.add(num)
+    return False
+
+def hash_approach(nums):
+    hashmap = {}
+    for num in nums:
+        if num in hashmap:
+            return True
+        hashmap[num] = 1
+    return False
+
+if __name__ == '__main__':
+    test_cases = [
+        ([1,2,3,1], True),
+        ([1,2,3,4], False),
+        ([], False),
+        ([7], False),
+        ([1,1,1,1,1], True),
+        (list(range(1000)), False),
+        ([i % 10 for i in range(1000)], True)
+    ]
+
+    for i in range(len(test_cases)):
+        (nums, result) = test_cases[i]
+        bf = bruteforce(nums)
+        set1 = set_approach1(nums)
+        set2 = set_approach2(nums)
+        hash = hash_approach(nums)
+
+
+        print("Brute Force \n Expected output: ",result, "\nActual output: ",bf )
+        # print("Set 1 \n Expected output: ",result, "\nActual output: ",set1 )
+        # print("Set 2 \n Expected output: ",result, "\nActual output: ",set2 )
+        # print("Hash \n Expected output: ",result, "\nActual output: ",hash )
+        
