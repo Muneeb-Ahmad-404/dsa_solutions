eafd596febdeca30b186e78d2d93af1585067715
Muneeb Ahmad <muneebahmad2005p@gmail.com>
2025-07-14 20:58:58 +0500

    added a testing class to avoid redundancy

---
+++ testing.py
@@ -0,0 +1,6 @@
+def test(self,  testcases, functions):
+    for i in range(len(testcases)):
+        for function in functions:
+            print(function.__name__)
+            result = function(testcases[i][0])
+            print(f'Test {i}  \nExpected: {testcases[i][1]} \nOutput: {result}')
